ea756c3e94ab135e25d840ddd5a6a6c087c3a5ec
kohei-shiomi <thisiskshiomi@gmail.com>
2022-11-28 14:55:31 +0900

    3章 練習問題1の修正
    3章 練習問題1の修正

--- 3-2/index.php
+++ 3-2/index.php
@@ -1,42 +1,19 @@
 <?php
 
-$fruits = ["150円のりんごが2つ", "50円のみかんが3つ", "300円のももが10個"];
-
-echo $fruits[0];
-echo '<br>';
-echo $fruits[1];
-echo '<br>';
-echo $fruits[2];
-?>
-
-<br><br>
-
-<?php
-function getSubtotal($fruits, $quantity){
-    $subtotal = $fruits * $quantity;
-    print $subtotal."円";
+$fruits = ["りんご" => 150, "みかん" => 30, "もも" => 500];
+$quantity = [2, 5 ,6];
+//
+function getSubtotal($price, $quantity){
+$subtotal = $price * $quantity;
+return $subtotal;
+echo $subtotal;
 }
-getSubtotal(150,2);
-echo'<br>';
-getSubtotal(50,3);
-echo'<br>';
-getSubtotal(300,10);
-?>
-
-<br><br>
-
-<?php
-$fruits = ["りんごは" => "300円です。", "みかんは" => "150円です。", "ももは" => "3000円です。"];
-
-foreach ($fruits as $key => $value){
-    echo $key;
-    echo $value;
-    echo '<br>';
+//
+$i = 0;
+foreach ($fruits as $key => $value) {
+$subtotal = getSubtotal($value, $quantity[$i]);
+print $key .'は'. $subtotal.'円です。'  .'<br />';
+++$i;
 }
 ?>
-
-
-
-
-
 
